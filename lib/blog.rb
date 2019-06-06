c03c3657237cd5cf3ad18fca979701c48c32ce02
Melanie Rogan <melanie.rogan@ft.com>
2019-06-06 10:30:34 +0100

    i broke something but now it is working again

--- lib/blog.rb
+++ lib/blog.rb
@@ -1,22 +1,22 @@
 require 'pg'
 
 class Blogger
-  attr_reader :id, :name, :message, :date
+  attr_reader :id, :name, :message, :created_on
 
-  def initialize(id, name, message, date)
+  def initialize(id, name, message, created_on)
     @id = id
     @name = name
     @message = message
-    @date = date
+    @created_on = created_on
   end
 
   def self.all
     connection = PG.connect(dbname: 'blogger')
-    result = connection.exec( "SELECT * FROM blogger;" )
-    result.map { |blogger| (blogger['id'], blogger['name'], blogger['message'],  blogger['date']) }
+    result = connection.exec( "SELECT * FROM blogger" )
+    result.map { |blogger| Blogger.new(blogger['id'], blogger['name'], blogger['message'],  blogger['created_on']) }
   end
 
   def to_s
-    "#{@id} #{@name} #{@message} #{@date}"
+    "#{@id} #{@name} #{@message} #{@created_on}"
   end
 end
